37ea92e11430a3a130916d1d2d6d63a0d12a8e4f
Jonas Schell <jonasschell@ocupe.org>
2017-08-31 01:46:45 +0200

    added test app for Table Widget

---
+++ table/table/app.py
@@ -0,0 +1,24 @@
+import toga
+from colosseum import CSS
+
+
+def build(app):
+    table = toga.Table(['Fist Heading', 'Second Heading'])
+    table.insert(None, 'root1', 'value1')
+    table.insert(None, 'root2', 'value2')
+    table.insert(None, 'root3', 'value3')
+    table.insert(0, 'root4', 'value4')
+
+    for _ in range(200):
+        table.insert(None, str(_), 'text')
+
+    return table
+
+
+def main():
+    return toga.App('Test Table', 'org.pybee.helloworld', startup=build)
+
+
+if __name__ == '__main__':
+    app = main()
+    app.main_loop()
