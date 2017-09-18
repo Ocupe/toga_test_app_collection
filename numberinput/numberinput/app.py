612ada9e92a5405d33a7d503fa8c8c0773aca56e
Ocupe <m@ocupe.org>
2017-09-18 19:48:20 +0200

    Updated numberinput

--- numberinput/numberinput/app.py
+++ numberinput/numberinput/app.py
@@ -7,15 +7,20 @@ from colosseum import CSS
 
 def build(app):
     box = toga.Box(style=CSS(flex=1, padding=20))
-    nuber_input = toga.NumberInput(min_value=100, max_value=200, step=1)
-    box.add(nuber_input)
+    number_input = toga.NumberInput(min_value=100, max_value=200, step=1)
+    box.add(number_input)
 
-    def callback(widget):
-        nuber_input.range = (0, 10)
+    def set_range(widget):
+        number_input.range = (0, 10)
 
-    btn = toga.Button('Set Range', on_press=callback, style=CSS())
+    btn = toga.Button('Set Range', on_press=set_range, style=CSS())
     box.add(btn)
 
+    def toggle_enabled(widget):
+        number_input.enabled = not number_input.enabled
+
+    toggle_btn = toga.Button('Toggle Enabled', on_press=toggle_enabled)
+    box.add(toggle_btn)
     return box
 
 
